3d8ed4afe91a15e4f440ccf6e8cd1c16206d621b
P7165387 <P7165387@INHYDWS3380.nessinhyd.com>
2018-07-04 17:05:18 +0530

    1

--- src/Test_Git/Test_Git.java
+++ src/Test_Git/Test_Git.java
@@ -4,7 +4,7 @@ public class Test_Git {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello World11224");
+System.out.println("Hello World11");
 	}
 
 }
